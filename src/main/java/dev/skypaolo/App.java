5541b68a02f7c055790930aa20caf1e1eb3c7d07
Antoine Mathié <antoine.mathie100@gmail.com>
2026-04-13 15:29:20 +0200

    feat: init javafx app

--- src/main/java/dev/skypaolo/App.java
+++ src/main/java/dev/skypaolo/App.java
@@ -1,8 +1,30 @@
 package dev.skypaolo;
 
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label label = new Label(
+            "Hello, JavaFX! Java version: " +
+                javaVersion +
+                ", JavaFX version: " +
+                javafxVersion
+        );
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 300, 200);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello from Zed + Java!");
+        launch();
     }
 }
