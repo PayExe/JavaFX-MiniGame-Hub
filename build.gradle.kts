edb6844ecda17de5e2229f63253c8d6caffba753
Antoine Mathié <antoine.mathie100@gmail.com>
2026-04-13 14:55:30 +0200

    feat: added javafx to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,29 @@
 plugins {
     application
     java
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 repositories {
     mavenCentral()
 }
 
-dependencies {}
+dependencies {
+    // JavaFX modules are managed by the plugin, but you can override versions here if needed
+}
+
+javafx {
+    version = "25"
+    modules(
+        "javafx.base",
+        "javafx.graphics",
+        "javafx.controls",
+        "javafx.fxml",
+        "javafx.media",
+        "javafx.web",
+        "javafx.swing"
+    )
+}
 
 application {
     mainClass = "app.App"
